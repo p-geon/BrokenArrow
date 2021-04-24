20071848e2fc683e31c4d04a9fa388df21bbda76
p-geon <nagano@nlab.ci.i.u-tokyo.ac.jp>
2021-04-24 21:55:37 +0900

    infinitfix

--- ansible/scripts/install-docker.sh
+++ ansible/scripts/install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## add GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
