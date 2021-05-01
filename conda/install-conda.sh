17bf9e23442f74826a93730d18a31f39f8cd0b87
p-geon <nagano@nlab.ci.i.u-tokyo.ac.jp>
2021-05-01 22:09:45 +0900

    add conda

---
+++ conda/install-conda.sh
@@ -0,0 +1,12 @@
+# download
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p ${HOME}/.miniconda
+rm miniconda.sh
+
+# PATH
+echo PATH="$PATH":${HOME}/.miniconda/bin >> ~/.bashrc
+conda info -e
+
+# activate
+conda create -n tf python=3.8
+conda activate tf
